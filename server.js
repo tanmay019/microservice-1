b20294f5d9dc38a57656276e511497bc5fbb4dee
tanmay019 <tanmaygautam999@outlook.com>
2020-08-24 03:21:57 +0530

    updated server.js

--- server.js
+++ server.js
@@ -18,25 +18,52 @@ const server = http.createServer(function (req, res) {
   //   return res.end("Unextpected shit occoured");
   // }
 
-  console.log(21, url, req.url);
-  if (url === "/react") {
-    console.log(23, url, req.url);
-    url = index;
-  }
-  console.log(26, url, req.url);
-  console.log(27, 'does path exist', fs.existsSync(dir+url) );
-  fs.readFile(dir + url, function (err, data) {
-    if (err) {
-      console.log(1, "Logging Unsuccesful @", url, req.url, new Date());
-      console.log(2, "Error and data is", err, data, new Date());
-      res.writeHead(200);
-      res.end("Unextpected shit occoured with url: "+req.url);
-    }
+  // console.log(21, url, req.url);
+  // if (url === "/react") {
+  //   console.log(23, url, req.url);
+  //   url = index;
+  // }
+  // console.log(26, url, req.url);
+  // console.log(27, 'does path exist', fs.existsSync(dir+url) );
+  // fs.readFile(dir + url, function (err, data) {
+  //   if (err) {
+  //     console.log(1, "Logging Unsuccesful @", url, req.url, new Date());
+  //     console.log(2, "Error and data is", err, data, new Date());
+  //     res.writeHead(200);
+  //     res.end("Unextpected shit occoured with url: "+req.url);
+  //   }
 
-    console.log(34, dir + url, data);
+  //   console.log(34, dir + url, data);
+  //   res.writeHead(200);
+  //   res.end(ms1);
+  // });
+
+  const ms1 = `
+  .::       .:: .::     .::    .:::::::         .::::        .:: ::   .:::::::: .:::::::     .::         .:: .::     .::    .::::::::               
+  .: .::   .::: .::  .::   .:: .::    .::     .::    .::   .::    .:: .::       .::    .::    .::       .::  .::  .::   .:: .::                .::  
+  .:: .:: . .:: .:: .::        .::    .::   .::        .::  .::       .::       .::    .::     .::     .::   .:: .::        .::                 .:: 
+  .::  .::  .:: .:: .::        .: .::       .::        .::    .::     .::::::   .: .::          .::   .::    .:: .::        .::::::    .:::::   .:: 
+  .::   .:  .:: .:: .::        .::  .::     .::        .::       .::  .::       .::  .::         .:: .::     .:: .::        .::                 .:: 
+  .::       .:: .::  .::   .:: .::    .::     .::     .::  .::    .:: .::       .::    .::        .::::      .::  .::   .:: .::                 .:: 
+  .::       .:: .::    .::::   .::      .::     .::::        .:: ::   .:::::::: .::      .::       .::       .::    .::::   .::::::::          .::::
+                                                                                                                                                        `;
+  const e404 = `
+                                      
+  .:::::::: .:::::::     .:::::::         .::::      .:::::::                                                  
+  .::       .::    .::   .::    .::     .::    .::   .::    .::                .::        .::            .::   
+  .::       .::    .::   .::    .::   .::        .:: .::    .::              . .::      .::  .::       . .::   
+  .::::::   .: .::       .: .::       .::        .:: .: .::       .:::::    .: .::    .::     .::     .: .::   
+  .::       .::  .::     .::  .::     .::        .:: .::  .::             .::  .::    .::      .::  .::  .::   
+  .::       .::    .::   .::    .::     .::     .::  .::    .::          .:::: .: .::  .::    .::  .:::: .: .::
+  .:::::::: .::      .:: .::      .::     .::::      .::      .::              .::       .:::            .::   
+                                                                                                                 `;
+
+  if (url === "/react") {
     res.writeHead(200);
-    res.end(data);
-  });
+    return res.end(ms1);
+  }
+  res.writeHead(200);
+  return res.end(e404);
 });
 
 server.listen(port);
