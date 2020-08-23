31129b52818a6d06efcef988ed4d431b3c4b3049
tanmay019 <tanmaygautam999@outlook.com>
2020-08-24 01:43:48 +0530

    ...

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const http = require('http');
-const path = require('path');
-const fetch = require('node-fetch');
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const fetch = require("node-fetch");
 const port = 3030;
 
-const deployFolder = 'public';
+const deployFolder = "public";
 const cwd = process.cwd();
 const dir = path.join(cwd, deployFolder);
-const index = '/index.html';
+const index = "/index.html";
 
 const server = http.createServer(function (req, res) {
   let url = req.url;
@@ -19,21 +19,22 @@ const server = http.createServer(function (req, res) {
   // }
 
   console.log(21, url, req.url);
-  if (url === '/react') {
+  if (url === "/react") {
     console.log(23, url, req.url);
-    url=index;
+    url = index;
   }
   console.log(26, url, req.url);
-  fs.readFile( dir + url, function (err, data) {
+  fs.readFile(dir + url, function (err, data) {
     if (err) {
-      console.log(1 ,"Logging Unsuccesful @", url, req.url, new Date());
-      console.log(2 ,"Error and data is", err, data, new Date());
+      console.log(1, "Logging Unsuccesful @", url, req.url, new Date());
+      console.log(2, "Error and data is", err, data, new Date());
       res.writeHead(200);
       return res.end("Unextpected shit occoured");
     }
-  res.writeHead(200);
-  return res.end(data);
-      });
+    console.log(34, dir+url, data);
+    res.writeHead(200);
+    return res.end(data);
+  });
 });
 
 server.listen(port);
