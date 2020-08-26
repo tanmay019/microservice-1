37349b74c0bdb75a652e3f7bffdfcc8c2d9f9cfe
tanmay019 <tanmaygautam999@outlook.com>
2020-08-26 06:40:14 +0530

    Integrated Helm charts

--- kubefiles/deploy-to-k8s.sh
+++ kubefiles/deploy-to-k8s.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 # kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
+releaseVersion=$1
+echo "The release version we are dealing with is: $releaseVersion"
 
 baseDir=$(echo $(pwd))
 kubeFiles=$baseDir/kubefiles
 
 cd $kubeFiles
 
-# kubectl apply -f ./ingress/net-ingress.yaml
+helm install $releaseVersion ./mychart
 
-kubectl apply -f ./deployment/log-deployment.yaml &&
-kubectl apply -f ./deployment/nginx-controller.yaml &&
-kubectl apply -f ./deployment/react-deployment.yaml
+# # kubectl apply -f ./ingress/net-ingress.yaml
 
-kubectl apply -f ./service/log-service.yaml &&
-kubectl apply -f ./service/nginx-service.yaml &&
-kubectl apply -f ./service/react-service.yaml
+# kubectl apply -f ./deployment/log-deployment.yaml &&
+# kubectl apply -f ./deployment/nginx-controller.yaml &&
+# kubectl apply -f ./deployment/react-deployment.yaml
 
-kubectl apply -f ./ingress/ingress.yaml
+# kubectl apply -f ./service/log-service.yaml &&
+# kubectl apply -f ./service/nginx-service.yaml &&
+# kubectl apply -f ./service/react-service.yaml
+
+# kubectl apply -f ./ingress/ingress.yaml
